d2e1b988009aa85b5a95db6bbd8ce54f476f122f
Dom.SEngineer <dominic.allen998@gmail.com>
2025-02-13 09:27:16 -0800

    testing...

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.plaf.synth.ColorType;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Cross platform");
-    }
+        public static void main(String[] args) {
+            JFrame window = new JFrame(); //sets the window
+            window.setBounds(500, 500, 500, 500);
+            window.setVisible(true);
+            JPanel panel1 = new JPanel();
+            panel1.setBounds(50, 500, 50, 300);
+
+            window.add(panel1);
+            window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        }
 }
 /*
-Nathan
-Tre
-Dominic
-Carlie
+Dominic's Branch
 */
