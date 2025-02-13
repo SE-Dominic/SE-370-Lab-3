b2b72582d4a9b2b2c893aa063a83549d915e891d
Dom.SEngineer <dominic.allen998@gmail.com>
2025-02-13 09:52:48 -0800

    pushing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import jdk.jfr.Unsigned;
+
 import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.io.*;
@@ -5,14 +7,26 @@ import javax.swing.*;
 import javax.swing.plaf.synth.ColorType;
 
 public class Main {
-        public static void main(String[] args) {
-            JFrame window = new JFrame(); //sets the window
-            window.setBounds(500, 500, 500, 500);
-            window.setVisible(true);
-            JPanel panel1 = new JPanel();
-            panel1.setBounds(50, 500, 100, 300);
-            window.add(panel1);
-            window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame(); //sets the window
+        window.setBounds(500, 500, 500, 500);
+        window.setVisible(true);
+
+        Draw drawPanel = new Draw(500, 500, 500, 500);
+        drawPanel.setVisible(true);
+        window.add(drawPanel);
+
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+//            JPanel panel1 = new JPanel();
+//            panel1.setBounds(50, 500, 100, 300);
+//            Graphics g;
+//
+//            //panel1;
+//            window.add(panel1);
+//            window.paintComponents(graphics);
+
         }
 }
 /*
